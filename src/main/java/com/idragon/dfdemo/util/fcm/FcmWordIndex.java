c18eb06f6a544ee35d63cb7d15fa94ebb4b3504d
陈新俊 <chenxj7@asiainfo.com>
2020-04-18 16:05:04 +0800

    提交模版生成工具，完成实体生成编写工作

--- src/main/java/com/idragon/dfdemo/util/fcm/FcmWordIndex.java
+++ src/main/java/com/idragon/dfdemo/util/fcm/FcmWordIndex.java
@@ -3,7 +3,6 @@ package com.idragon.dfdemo.util.fcm;
 import com.alibaba.fastjson.JSONObject;
 import com.idragon.dfdemo.util.ExcelUtils;
 import com.idragon.dfdemo.util.WordUtils;
-import com.idragon.dfdemo.util.fcm.FcmDataUtils;
 import com.idragon.dfdemo.util.fcm.dto.BeanInfo;
 import com.idragon.dfdemo.util.fcm.word.BeanInfoWordUtils;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -17,11 +16,15 @@ import java.util.List;
  */
 public class FcmWordIndex {
     public static void main(String[] args) throws Exception {
-        JSONObject data= ExcelUtils.getExcelData("/Users/chenxinjun/Downloads/930.xlsx");
-        List<BeanInfo> list = FcmDataUtils.getBeanInfos(data);
-        String wordModelName="/Users/chenxinjun/Downloads/model.docx";
+        //原始录入数据
+        String sourceData="/Users/chenxinjun/Downloads/930.xlsx";
+        // doc文档输出工具
         String targetName = "/Users/chenxinjun/Downloads/hh.docx";
-        String beanModel = "/Users/chenxinjun/Downloads/beanModel.docx";
+        // 解析录入数据，生成对应实体
+        JSONObject data= ExcelUtils.getExcelData(sourceData);
+        List<BeanInfo> list = FcmDataUtils.getBeanInfos(data);
+        String beanModel = FcmWordIndex.class.getResource("/").getPath()+"/doc/beanModel.docx";
+
         XWPFDocument document = BeanInfoWordUtils.getBeanListDoc(beanModel, list);
         WordUtils utils=new WordUtils();
         utils.exportFile(document,targetName);
